096d89588bbdce3ec6517c19a458bc614842d7ce
jpc <jph.constantin1217@gmail.com>
2020-07-03 13:45:40 +0200

    version 0.54

--- deploy.sh
+++ deploy.sh
@@ -6,5 +6,5 @@ git config user.name "Travis CI"
 git config user.email "travis@travis-ci.org"
 git add --all
 git commit --message  "Auto deploy from Travis CI build $TRAVIS_BUILD_NUMBER"
-git remote add deploy https://$GH_TOKEN@github.com/jphconstantin/jphconstantin.github.io.git >/dev/null 2>&1
+git remote add deploy https://$GH_TOKEN@github.com/jphconstantin/faune_flore_meyrin.github.io.git >/dev/null 2>&1
 git push --force deploy gh-pages >/dev/null 2>&1
